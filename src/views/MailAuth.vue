4e44ca9828bc961db28c494f724f8dfdcf8b08e6
km <km@kmnoMacBook-Pro.local>
2020-12-13 14:54:17 +0900

    必要な記述を追加

--- src/views/MailAuth.vue
+++ src/views/MailAuth.vue
@@ -3,38 +3,41 @@
     <h1>aaaa</h1>
     <form>
       <span>ID</span>
-      <input 
-        type="email" 
-        v-model="email"
-      />
+      <input type="email" v-model="email" />
       <span>PW</span>
-      <input 
-        type="password"
-        v-model="password" 
-      />
-      <button 
-        type="button"
-        @click="mailLogin"
-      >ログイン</button>
+      <input type="password" v-model="password" />
+      <button type="button" @click="mailLogin">ログイン</button>
     </form>
   </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        email: '',
-        password: '',
-      }
+import firebase from 'firebase'
+
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    mailLogin() {
+      console.log(this.email);
+      console.log(this.password);
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.email, this.password)
+        .then( function(res) {
+          console.log(res)
+          alert("ログインできました");
+        })
+        .catch(function (error) {
+          alert("ログインできません（" + error.message + "）");
+        });
     },
-    methods: {
-      mailLogin() {
-        console.log(this.email)
-        console.log(this.password)
-      },
-    }
-  }
+  },
+};
 </script>
 
 <style scoped>
