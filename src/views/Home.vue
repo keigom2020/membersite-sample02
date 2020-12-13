87178ed8a55e1faff3fb5f3f88a9b09262a8297f
km <km@kmnoMacBook-Pro.local>
2020-12-13 15:07:24 +0900

    googleのログインボタンを設置しているところにメールアドレス用のログインボタンを追加

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,9 +3,32 @@
     <div class="container">
       <img src="@/assets/img/logo.png" alt="ロゴ">
       <div class="row">
-        <h1>会員サイト</h1>
+        <h1 class="heading__lv1">会員サイト</h1>
         <p class="message">会員サイトをご利用の方は、Googleアカウントでログインしてください。</p>
       </div>
+
+      <h2 class="heading__lv2">メールアドレスでログイン</h2>
+      <form>
+        <div class="form__item">
+          <p class="form__label">Email</p>
+          <input type="email" v-model="email" />
+        </div>
+        <div class="form__item">
+          <p class="form__label">Password</p>
+          <input 
+            type="password" 
+            v-model="password" 
+          />
+        </div>
+
+        <button 
+          type="button" 
+          @click="mailLogin"
+          class="btn"
+        >メールアドレスでログイン</button>
+      </form>
+
+      <h2 class="heading__lv2">Googleアカウントでログイン</h2>
       <button 
         class="btn"
         @click="login"
@@ -19,25 +42,45 @@
 <script>
 // @ is an alias to /src
 import { mapActions } from 'vuex'
+import firebase from 'firebase'
 
 export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
   methods: {
-    ...mapActions(['login'])
-  }
+    ...mapActions(['login']),
+    mailLogin() {
+      console.log(this.email);
+      console.log(this.password);
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.email, this.password)
+        .then(function(res) {
+          console.log(res)
+          alert("ログインできました");
+        })
+        .catch(function (error) {
+          alert("ログインできません（" + error.message + "）");
+        });
+    },
+  },
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 body {
   color: #fff;
 }
 
-p {
-  color: #fff;
-}
-
-h1 {
-  color: #fff;
+.heading {
+  &__lv2 {
+    margin-bottom: 0rem;
+    margin-top: 2rem;
+  }
 }
 
 #home .message {
@@ -45,8 +88,38 @@ h1 {
   font-size: 1rem;
 }
 
-@media screen and(max-width: 767px) {
+.form__item {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 10px;
+  color: #fff;
+}
 
+.form__label {
+  color: #fff;
+  min-width: 120px;
+  text-align: left;
+  font-size: 1.4rem;
+
+  ~ input {
+    min-width: 300px;
+  }
+}
+
+@media screen and(max-width: 767px) {
+  .heading {
+    &__lv2 {
+      font-size: 1.4rem;
+    }
+  }
+  
+
+  .form__label {
+
+    ~ input {
+      min-width: 50%;
+    }
+  }
 }
 
 </style>
